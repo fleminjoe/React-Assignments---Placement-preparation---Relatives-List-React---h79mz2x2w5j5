59814495891fbecc5265e14f5738c2470450cedb
fleminjoe <fleminjoe@gmail.com>
2021-09-06 00:20:47 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,24 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    constructor(){
+        super()
+        this.relativeList = ["Lucy", "Jiji"]
+    }
+
+    
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               
+               <ol>
+                {
+                    this.relativeList.map((relativeListItem,index)=>{
+                    return <li key={`relativeListItem${index+1}`}>{relativeListItem}</li>})
+                }
+               </ol>
             </div>
         )
     }
